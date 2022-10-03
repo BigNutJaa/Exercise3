2cdc10ecadca8b61b2f971d64e85f71f9e313ed8
BigNutJaa <akkhaphat.i@hotmail.com>
2022-10-03 14:24:45 +0700

    newCode

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Exercise3
 
 go 1.19
+
+require github.com/BigNutJaa/sortDESC v0.0.2
--- main.go
+++ main.go
@@ -1,46 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"sort"
-	"strconv"
+	"github.com/BigNutJaa/sortDESC"
+	"log"
 )
 
-var wording string
-
-type sortRunes []rune
-
-func (s sortRunes) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
-func (s sortRunes) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s sortRunes) Len() int {
-	return len(s)
-}
-
-func SortString(s string) string {
-	r := []rune(s)
-	sort.Sort(sortRunes(r))
-	return string(r)
-}
-
 func main() {
-	for {
-		fmt.Println("Enter some word:")
-		fmt.Scanln(&wording)
-		if _, err := strconv.ParseFloat(wording, 64); err == nil {
-			fmt.Printf("%q looks like a number.\n", wording)
-		} else {
-			break
 
-		}
+	result, err := sortDESC.Sorta("bignut")
+	if err != nil {
+		log.Println("error:", err)
 	}
-	newWording := SortString(wording)
-	fmt.Println("Input word =", wording)
-	fmt.Println("New word(sort desc) =", newWording)
-
+	log.Println(result)
 }
